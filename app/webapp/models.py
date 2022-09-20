3cff2eaa3fb67434f0cbac2fd9924f2aa9a98def
Irina Ten <virhot17@gmail.com>
2022-09-20 16:04:19 +0600

    Improvement
    Улучшена страница основного списка. Данные выходят в виде таблицы.

--- app/webapp/models.py
+++ app/webapp/models.py
@@ -9,7 +9,7 @@ class ToDo(models.Model):
     )
     text = models.TextField(verbose_name="Description", max_length=500, null=False, blank=False)
     status = models.CharField(verbose_name="Status", max_length=50, null=False, default="New", choices=CHOICES)
-    completion_data = models.DateField(verbose_name="Date of completion", null=True, default=None)
+    completion_data = models.TextField(verbose_name="Date of completion", null=True, default=None)
 
     def __str__(self):
         return f"{self.status} - {self.text}"
