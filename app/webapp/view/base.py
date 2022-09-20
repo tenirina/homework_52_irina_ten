0fc99986da615101079b7344ea11213578a8e363
Irina Ten <virhot17@gmail.com>
2022-09-20 18:40:52 +0600

    Improvement
    Исправлена ошибка в отображении статуса на экране на основной старнице

--- app/webapp/view/base.py
+++ app/webapp/view/base.py
@@ -6,8 +6,7 @@ from webapp.models import ToDo
 def index_view(request):
     to_does = ToDo.objects.all()
     for el in to_does:
-        if el.status == "new":
-            el.status = el.get_status_display()
+        el.status = el.get_status_display()
     context = {
         "to_does": to_does
     }
